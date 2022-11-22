300b6007dee43dfb3696b954ac6b1e36841cd059
Cesar Moros <rdcesar442@gmail.com>
2022-11-22 13:27:13 -0300

    changed loader

--- public/js/modules/Spin.js
+++ public/js/modules/Spin.js
@@ -1,6 +1,16 @@
 const container = document.createElement("div");
 container.classList.add("spin__container");
-container.innerHTML = `<div class="spin">`;
+container.innerHTML = `<div class="loading-spinner-container">
+<div class="loading-spinner">
+<div class="loading-spinner-block loading-spinner-block--1"></div>
+<div class="loading-spinner-block loading-spinner-block--2"></div>
+<div class="loading-spinner-block loading-spinner-block--3"></div>
+<div class="loading-spinner-block loading-spinner-block--4"></div>
+<div class="loading-spinner-block loading-spinner-block--5"></div>
+<div class="loading-spinner-block loading-spinner-block--6"></div>
+<div class="loading-spinner-block loading-spinner-block--7"></div>
+<div class="loading-spinner-block loading-spinner-block--8"></div>
+</div></div>`;
 
 const body = document.querySelector("body");
 
